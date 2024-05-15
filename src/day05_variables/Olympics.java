75521f66b9594b81c2bb73987994322f3cabcf0e
Maksym <martynenkomaksim1983@gmail.com>
2024-05-15 19:03:01 -0400

    some more methods multi demenhenal array

---
+++ src/day05_variables/Olympics.java
@@ -0,0 +1,21 @@
+package day05_variables;
+
+public class Olympics {
+    public static void main(String[] args) {
+        int num1 = 5;
+        num1 = 4;
+        int num2 = 7;
+        System.out.println(num1);  // 4
+        num2 = num1;
+        System.out.println(num2);  // 4
+
+        System.out.println("" + 3+3 + "1234" + 5); // 3312345
+        System.out.println(1234);   //1234
+
+        System.out.println(" ");
+        System.out.println("");
+
+        byte n = 4;
+        System.out.println(n + 2);
+    }
+}
